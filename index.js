24bea92475f51e82e86bf588429fc1457e9b9a92
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-11-11 22:30:58 +0600

    done now

--- index.js
+++ index.js
@@ -6,26 +6,39 @@ const app = express()
 const port = process.env.PORT || 5000;
  
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzobjs3.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: true, 
     deprecationErrors: true,
   }
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   
+   client.connect();
+      const petsCollection = client.db('pets').collection('allPets');
+       app.get('/pets', async (req, res) => {
+      const cursor = petsCollection.find({});
+      const pets = await cursor.toArray();
+
+      res.send(pets);
+       });
+      app.get('/pets/:id', async (req, res) => {
+      const id = req.params.id;
+     const result = await petsCollection.findOne({ _id: new ObjectId(id) });
+     
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
